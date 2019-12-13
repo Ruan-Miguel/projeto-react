e635779121b993dca9c4ea9acb8cf82f8a9480ea
Ruan-Miguel <ruanmiguel14@hotmail.com>
2019-12-13 10:50:05 -0300

    animacao funcionando ao clicar na cor

--- src/components/Bloco/index.js
+++ src/components/Bloco/index.js
@@ -1,25 +1,42 @@
 import React, { Component } from 'react'
+import styled, { keyframes } from "styled-components";
 
 import './styles.css'
 
 export default class Bloco extends Component {
-    constructor (props) {
-        super(props)
+    
+    componentWillUpdate (prevProps) {
+        if (prevProps.cor !== this.props.cor) {
+            const animacao = keyframes`{
+                0% {background: radial-gradient(circle, ${prevProps.cor} 0%, ${this.props.cor} 0%)}
+                25% {background: radial-gradient(circle, ${prevProps.cor} 25%, ${this.props.cor} 0%)}
+                50% {background: radial-gradient(circle, ${prevProps.cor} 50%, ${this.props.cor} 0%)}
+                75% {background: radial-gradient(circle, ${prevProps.cor} 75%, ${this.props.cor} 0%)}
+                100% {background: radial-gradient(circle, ${prevProps.cor} 100%, ${this.props.cor} 0%)}
+            }
+            `
 
-        this.state = {
-            corAnterior: null,
+            this.Div = styled.div`
+            background: ${this.props.cor};
+            animation: ${animacao} 200ms forwards;
+            `
+            
         }
     }
 
+    componentWillMount () {
+        this.Div = styled.div`
+        background: ${this.props.cor};
+        `
+    }
+
+    Div = null
+
     render () {
         return (
-            <div style={
-                Object.assign(
-                    this.props.estilo,
-                    {background: this.props.cor}
-                )}
+            <this.Div style={ this.props.estilo }
             className='bloco'
-            onClick={this.props.handleClick} >{this.props.texto}</div>
+            onClick={this.props.handleClick} >{this.props.texto}</this.Div>
         )
     }
 }
